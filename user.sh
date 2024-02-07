3ae535a8864abb743808286628821a436651cf5e
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-07 12:30:13 +0530

    shellscript for user Module

---
+++ user.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+MONGODB_HOST=mongodb.techwithgopi.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script Started Executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+         echo -e "$2 ... $R FAILED $N"
+         exit 1
+    else
+         echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $? -ne 0 ]
+then
+     echo -e "$R ERROR :: Please run this script  with root access $N"
+     exit 1
+else
+     echo -e " You are root user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling the current Nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enabling Nodejs"
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Installing the Nodejs:18"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+     useradd roboshop
+     VALIDATE $? "roboshop user creation"
+else
+     echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
+
+VALIDATE $? "downloading user application"
+
+cd /app
+
+unzip /tmp/user.zip &>> $LOGFILE
+
+VALIDATE $? "Unzipping the application"
+
+npm install
+
+VALIDATE $? "Installing dependencies"
+
+cp /home/centos/roboshop-scripting/user.service /etc/systemd/system/user.service
+
+VALIDATE $? "Copying user service file"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "user deamon reload"
+
+systemctl enable user &>> $LOGFILE
+
+VALIDATE $? "Enabling user"
+
+systemctl start user &>> $LOGFILE
+
+VALIDATE $? "Starting user"
+
+cp /home/centos/roboshop-scripting/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "Copying mongo repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb client"
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js &>> $LOGFILE
+
+VALIDATE $? "Loading catalogue data into mongodb"
