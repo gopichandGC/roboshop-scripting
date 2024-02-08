0de3f3e944105705002e35b8010fd9cb216967ca
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-08 11:04:27 +0530

    Shell Script for EC2-Instance & Records

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI=ami-0f3c7d07486cad139
 SG_ID=sg-0523f705109b77eaa
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 ZONE_ID=Z01392352BS5MX7GMAHS1
-DOMAIN_NAME=
+DOMAIN_NAME="techwithgopi.online"
 
 for i in "${INSTANCES[@]}"
 do
@@ -19,4 +19,23 @@ do
 
     echo "$i: $IP_ADDRESS"
 
+#Creating new R53 record, Make sure you delete the existing record
+  aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch "
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "$i.$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP_ADDRESS"
+        }]
+    }
+    }]
+  }
+  "
 done
